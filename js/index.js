c8cd59081f86dbab234dde4575c84b67127b6912
Damián López <lopezrunco@gmail.com>
2022-08-05 11:44:55 +0530

    Contact form

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import injectAll from './injectHTML.js'
 import scrollToTop from './scrollToTop.js'
+import contactFormValidations from './contactFormValidations.js'
 
 const d = document
 
 d.addEventListener('DOMContentLoaded', e => {
     injectAll()
     scrollToTop('.scroll-top-btn')
+    contactFormValidations()
 })
